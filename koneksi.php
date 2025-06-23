a78ee97b9b72763be4f7cff751d48bdc47d0c58c
Ahmad Rizky <149795044+Ahmadrizky12xyz@users.noreply.github.com>
2025-06-24 01:45:42 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,16 @@
 <?php
-// Pastikan parameter sesuai dengan konfigurasi database Anda
-$conn = mysqli_connect('localhost', 'root', '', 'absensi_ptindotekhnoplus');
+$host = 'mysql-4ec7fa0-rizky15raditya-e894.d.aivencloud.com';
+$port = 21015;
+$dbname = 'defaultdb';
+$username = 'avnadmin';
+$password = 'AVNS_kDRQLgJnd6wDKbJmjgD';
 
-if (!$conn) {
-    die("Koneksi gagal: " . mysqli_connect_error());
+// Koneksi pakai PDO dan SSL
+$pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password, [
+    PDO::MYSQL_ATTR_SSL_CA => __DIR__ . '/ca.pem', // Download dari Aiven console
+]);
+
+// Test koneksi
+if ($pdo) {
+    echo "Koneksi ke Aiven MySQL sukses!";
 }
-?>
